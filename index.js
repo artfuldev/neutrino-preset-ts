7d29c6f8ad97ac4ef06e30472d00687be85d224b
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-04-22 22:13:45 +0530

    Make default entry as index.ts

--- index.js
+++ index.js
@@ -16,107 +16,109 @@ const { join, dirname } = require('path');
 const { path, pathOr } = require('ramda');
 
 const MODULES = join(__dirname, 'node_modules');
+const DEFAULT_ENTRY = join(__dirname, 'src/index.js');
+const DEFAULT_PRESET_ENTRY = join(__dirname, 'src/index.ts');
 
 function devServer({ config }, options) {
-  config.devServer
-    .host(options.host)
-    .port(parseInt(options.port, 10))
-    .https(options.https)
-    .contentBase(options.contentBase)
-    .historyApiFallback(true)
-    .hot(true)
-    .publicPath('/')
-    .stats({
-      assets: false,
-      children: false,
-      chunks: false,
-      colors: true,
-      errors: true,
-      errorDetails: true,
-      hash: false,
-      modules: false,
-      publicPath: false,
-      timings: false,
-      version: false,
-      warnings: true
-    });
+    config.devServer
+        .host(options.host)
+        .port(parseInt(options.port, 10))
+        .https(options.https)
+        .contentBase(options.contentBase)
+        .historyApiFallback(true)
+        .hot(true)
+        .publicPath('/')
+        .stats({
+            assets: false,
+            children: false,
+            chunks: false,
+            colors: true,
+            errors: true,
+            errorDetails: true,
+            hash: false,
+            modules: false,
+            publicPath: false,
+            timings: false,
+            version: false,
+            warnings: true
+        });
 }
 
 module.exports = (neutrino) => {
-  neutrino.use(env);
-  neutrino.use(htmlLoader);
-  neutrino.use(styleLoader);
-  neutrino.use(fontLoader);
-  neutrino.use(imageLoader);
-  neutrino.use(htmlTemplate, neutrino.options.html);
-  neutrino.use(namedModules);
-  neutrino.use(tsLoader);
-  neutrino.config
-    .when(process.env.NODE_ENV !== 'test', () => neutrino.use(chunk))
-    .target('web')
-    .context(neutrino.options.root)
-    .entry('index')
-      .add(neutrino.options.entry || './index.ts')
-      .end()
-    .output
-      .path(neutrino.options.output)
-      .publicPath('./')
-      .filename('[name].bundle.js')
-      .chunkFilename('[id].[chunkhash].js')
-      .end()
-    .resolve
-      .modules
+    neutrino.use(env);
+    neutrino.use(htmlLoader);
+    neutrino.use(styleLoader);
+    neutrino.use(fontLoader);
+    neutrino.use(imageLoader);
+    neutrino.use(htmlTemplate, neutrino.options.html);
+    neutrino.use(namedModules);
+    neutrino.use(tsLoader);
+    neutrino.config
+        .when(process.env.NODE_ENV !== 'test', () => neutrino.use(chunk))
+        .target('web')
+        .context(neutrino.options.root)
+        .entry('index')
+        .add(neutrino.options.entry === DEFAULT_ENTRY ? DEFAULT_PRESET_ENTRY : neutrino.options.entry)
+        .end()
+        .output
+        .path(neutrino.options.output)
+        .publicPath('./')
+        .filename('[name].bundle.js')
+        .chunkFilename('[id].[chunkhash].js')
+        .end()
+        .resolve
+        .modules
         .add('node_modules')
         .add(neutrino.options.node_modules)
         .add(MODULES)
         .end()
-      .extensions
+        .extensions
         .add('.js')
         .add('.json')
         .end()
-      .end()
-    .resolveLoader
-      .modules
+        .end()
+        .resolveLoader
+        .modules
         .add(neutrino.options.node_modules)
         .add(MODULES)
         .end()
-      .end()
-    .node
-      .set('console', false)
-      .set('global', true)
-      .set('process', true)
-      .set('Buffer', true)
-      .set('__filename', 'mock')
-      .set('__dirname', 'mock')
-      .set('setImmediate', true)
-      .set('fs', 'empty')
-      .set('tls', 'empty')
-      .end()
-    .when(process.env.NODE_ENV === 'development', (config) => {
-      const protocol = process.env.HTTPS ? 'https' : 'http';
-      const host = process.env.HOST || pathOr('localhost', ['options', 'config', 'devServer', 'host'], neutrino);
-      const port = process.env.PORT || pathOr(5000, ['options', 'config', 'devServer', 'port'], neutrino);
+        .end()
+        .node
+        .set('console', false)
+        .set('global', true)
+        .set('process', true)
+        .set('Buffer', true)
+        .set('__filename', 'mock')
+        .set('__dirname', 'mock')
+        .set('setImmediate', true)
+        .set('fs', 'empty')
+        .set('tls', 'empty')
+        .end()
+        .when(process.env.NODE_ENV === 'development', (config) => {
+            const protocol = process.env.HTTPS ? 'https' : 'http';
+            const host = process.env.HOST || pathOr('localhost', ['options', 'config', 'devServer', 'host'], neutrino);
+            const port = process.env.PORT || pathOr(5000, ['options', 'config', 'devServer', 'port'], neutrino);
 
-      neutrino.use(hot);
-      neutrino.use(devServer, {
-        host,
-        port,
-        https: pathOr(protocol === 'https', ['options', 'config', 'devServer', 'https'], neutrino),
-        contentBase: neutrino.options.source
-      });
+            neutrino.use(hot);
+            neutrino.use(devServer, {
+                host,
+                port,
+                https: pathOr(protocol === 'https', ['options', 'config', 'devServer', 'https'], neutrino),
+                contentBase: neutrino.options.source
+            });
 
-      config
-        .devtool('inline-source-map')
-        .entry('index')
-        .add(`webpack-dev-server/client?${protocol}://${host}:${port}/`)
-        .add('webpack/hot/dev-server');
-    }, (config) => {
-      neutrino.use(clean, { paths: [neutrino.options.output] });
-      neutrino.use(minify);
-      neutrino.use(copy, {
-        patterns: [{ context: neutrino.options.source, from: '**/*' }],
-        options: { ignore: ['*.js*', '*.ts*'] }
-      });
-      config.output.filename('[name].[chunkhash].bundle.js');
-    });
+            config
+                .devtool('inline-source-map')
+                .entry('index')
+                .add(`webpack-dev-server/client?${protocol}://${host}:${port}/`)
+                .add('webpack/hot/dev-server');
+        }, (config) => {
+            neutrino.use(clean, { paths: [neutrino.options.output] });
+            neutrino.use(minify);
+            neutrino.use(copy, {
+                patterns: [{ context: neutrino.options.source, from: '**/*' }],
+                options: { ignore: ['*.js*', '*.ts*'] }
+            });
+            config.output.filename('[name].[chunkhash].bundle.js');
+        });
 };
--- tests/ts.test.js
+++ tests/ts.test.js
@@ -8,28 +8,59 @@ test('loads preset', t => {
 
 test('uses preset', t => {
   const api = Neutrino();
-
   t.notThrows(() => api.use(require('..')));
 });
 
 test('valid preset production', t => {
   process.env.NODE_ENV = 'production';
   const api = Neutrino();
-
   api.use(require('..'));
-
-  const errors = validate(api.config.toConfig());
-
+  const config = api.config.toConfig();
+  // console.log(config);
+  const errors = validate(config);
   t.is(errors.length, 0);
 });
 
 test('valid preset development', t => {
   process.env.NODE_ENV = 'development';
   const api = Neutrino();
-
   api.use(require('..'));
-
-  const errors = validate(api.config.toConfig());
-
+  const config = api.config.toConfig();
+  // console.log(config);
+  const errors = validate(config);
   t.is(errors.length, 0);
+});
+
+test('preset development has given entry if available', t => {
+  process.env.NODE_ENV = 'development';
+  const options = { entry: 'index.xyz' };
+  const api = Neutrino(options);
+  api.use(require('..'));
+  const config = api.config.toConfig();
+  t.true(config.entry.index[0].endsWith('index.xyz'));
+});
+
+test('preset production has given entry if available', t => {
+  process.env.NODE_ENV = 'production';
+  const options = { entry: 'index.xyz' };
+  const api = Neutrino(options);
+  api.use(require('..'));
+  const config = api.config.toConfig();
+  t.true(config.entry.index[0].endsWith('index.xyz'));
+});
+
+test('preset development entry defaults to index.ts', t => {
+  process.env.NODE_ENV = 'development';
+  const api = Neutrino();
+  api.use(require('..'));
+  const config = api.config.toConfig();
+  t.true(config.entry.index[0].endsWith('index.ts'));
+});
+
+test('preset production entry defaults to index.ts', t => {
+  process.env.NODE_ENV = 'production';
+  const api = Neutrino();
+  api.use(require('..'));
+  const config = api.config.toConfig();
+  t.true(config.entry.index[0].endsWith('index.ts'));
 });
